35229831c657d3d03720d33e414ff2ae22122f47
Rafid Islam <rafidislam529@gmail.com>
2024-05-09 03:35:33 +0600

    added computer messages

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("rock paper xxx")
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3)
+    
+}
+rock = "The computer chose rock"
+paper = "The computer chose paper"
+scissor = "The computer chose scissor"
+
+result = getComputerChoice()
+console.log(result)
+
+result === 0 ? console.log(rock):
+result === 1? console.log(paper):
+console.log(scissor);
+
