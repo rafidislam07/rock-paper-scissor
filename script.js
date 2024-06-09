4246eb509211574d78bedc2fad027ab432eea8a7
Rafid Islam <rafidislam529@gmail.com>
2024-06-09 23:41:14 +0600

    made pretty colors

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ const result2 = document.getElementById("result2")
 const result3 = document.getElementById("result3")
 const choices = document.querySelectorAll(".choice")
 
-
+intro.style.fontFamily = 'Courier New';
+intro.style.fontSize = "50px"
+result1.style.color = "purple";                          
+result1.style.fontFamily = 'Courier New';
+result1.style.fontSize = "30px"
+result2.style.color = "purple";                          
+result2.style.fontFamily = 'Courier New';
+result2.style.fontSize = "30px"
+result3.style.color = "purple";                          
+result3.style.fontFamily = 'Courier New';
+result3.style.fontSize = "30px"     
+computerAnswer.style.color = "purple";                          
+computerAnswer.style.fontFamily = 'Courier New';
+computerAnswer.style.fontSize = "30px"  
 
 
 choices.forEach((choice) => {
@@ -57,13 +70,14 @@ function displayResult() {
       (humanSelection === "paper" && computerSelection === "rock") ||
       (humanSelection === "scissor" && computerSelection === "paper")
     ) {
-      result1.textContent = "youuuu winnn yayyayaya let's go babyy";
+      result1.textContent = "youuuu winnn yayyayaya let's go babyy";  
       result2.textContent = `You: ` + ++humanScore;
       result3.textContent = `Rafid: ` + computerScore;
     } else {
       result1.textContent = "awww you didnt winn its okiee ii still love youu jaan";
       result2.textContent =`You: ` + humanScore;
       result3.textContent =`Rafid: ` + ++computerScore;
+
 
 
     }
